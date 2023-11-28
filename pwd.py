79cea2ab3fb88dc490afd6690f78482a2e18d5e1
Ethan Cheung <54164738+echeung801@users.noreply.github.com>
2023-11-27 21:00:07 -0500

    Update pwd.py
    Added a while loop in case user wanted to generate multiple passwords.

--- pwd.py
+++ pwd.py
@@ -1,37 +1,47 @@
 import random
 
+# This is a program made by Ethan C. to help generate passwords for different job portal accounts
+
+
+QUIT = False;
+
 ALPHABET = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
 NUMBERS = ["0","1","2","3","4","5","6","7","8","9"]
-
 SYMBOLS = ["!","@","#","$","%","&","?"]
-
 string = "" # initializing string
 
 
-length = int(input("Choose how long you want your new password to be. ")) # user prompt
 
+while not QUIT:
+        length = int(input("Choose how long you want your new password to be. ")) # user prompt
 
-for i in range(0, length): # for loop
-    
-    add = "" # the upper or lowercase letter / symbol being concatenated
-    
-    whichArray = int((random.random() * (1000 + random.random())) % 4) # random case chosen
-    
-    match whichArray: # switch case
-        case 0: # letter case lowercase
-                add = ALPHABET[int((random.random() * 1000 + random.random()%2 + random.random())) % 26]
-        case 1: # letter case uppercase
-                add = (ALPHABET[int((random.random() * 1000 + random.random()%3 + random.random())) % 26]).upper()
+        for i in range(0, length): # for loop
+
+                add = "" # the upper or lowercase letter / symbol being concatenated                
+                whichArray = int((random.random() * (1000 + random.random())) % 4) # random case chosen
                 
-        case 2: # alphanumeric case
-                add = SYMBOLS[int((random.random() * 1000 + random.random()%4 + random.random())) % 7]
-        
-        case 3: # number case
-                add = NUMBERS[int((random.random() * 1000 + random.random()%3 + random.random())) % 10]
-        
-        
-    string = string + add #concatenate string
+                match whichArray: # switch case
+                        case 0: # letter case lowercase
+                                add = ALPHABET[int((random.random() * 1000 + random.random()%2 + random.random())) % 26]
+                        case 1: # letter case uppercase
+                                add = (ALPHABET[int((random.random() * 1000 + random.random()%3 + random.random())) % 26]).upper()
+                                
+                        case 2: # alphanumeric case
+                                add = SYMBOLS[int((random.random() * 1000 + random.random()%4 + random.random())) % 7]
+                        
+                        case 3: # number case
+                                add = NUMBERS[int((random.random() * 1000 + random.random()%3 + random.random())) % 10]
+                        
+                string = string + add #concatenate string
 
     
-print("Here is your new password: ", string, "\n")
+        print("Here is your new password: ", string, "\n")
+        q = input("Would you like to generate another password? Enter 'q' to exit. \n")
+      
+        if(q == "q"):
+                QUIT = True
+
+        # else:
+        string = ""
+        
+print("bye!!!  :)")
