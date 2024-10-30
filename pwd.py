9b458beea75f0c6540901ee5cafa5189535ef0e8
Ethan Cheung <54164738+echeung801@users.noreply.github.com>
2024-10-30 15:26:46 -0400

    Update pwd.py
    Change library of choice from random to secrets for secure random number generation and selection

--- pwd.py
+++ pwd.py
@@ -1,48 +1,47 @@
-import random
+import secrets
 
-# This is a program made by Ethan C. to help generate passwords for different job portal accounts
+# This is a program made to help generate passwords for different job portal accounts
 
 
 QUIT = False;
 
 ALPHABET = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 NUMBERS = ["0","1","2","3","4","5","6","7","8","9"]
-SYMBOLS = ["!","@","#","$","%","&","?"]
-string = "" # initializing string
+SYMBOLS = ["!","@","#","$","%","?","*"]
+string = [] # initializing string
 
 
 
 while not QUIT:
-        length = int(input("Choose how long you want your new password to be. ")) # user prompt
+        length = int(input("Choose how long you want your new password to be. \n")) # user prompt
 
-        for i in range(0, length): # for loop
+        for i in range(length): # for loop
 
                 add = "" # the upper or lowercase letter / symbol being concatenated                
-                whichArray = int((random.random() * (1000 + random.random())) % 4) # random case chosen
+                whichArray = secrets.choice([0, 1, 2, 3])
                 
                 match whichArray: # switch case
                         case 0: # letter case lowercase
-                                add = ALPHABET[int((random.random() * 1000 + random.random()%2 + random.random())) % 26]
+                                string.append(secrets.choice(ALPHABET))
                         
                         case 1: # letter case uppercase
-                                add = (ALPHABET[int((random.random() * random.random() * 1000 + random.random()%3 + random.random())) % 26]).upper()
+                                string.append((secrets.choice(ALPHABET)).upper())
                                 
                         case 2: # alphanumeric case
-                                add = SYMBOLS[int((random.random() * random.random() * 1000 + random.random()%4 + random.random())) % 7]
+                                string.append(secrets.choice(SYMBOLS))
                         
                         case 3: # number case
-                                add = NUMBERS[int((random.random() * 1000 + random.random()%3 + random.random())) % 10]
+                                string.append(secrets.choice(NUMBERS))
                         
-                string = string + add #concatenate string
+        done = ''.join(string)
 
-    
-        print("Here is your new password: ", string, "\n")
-        q = input("Would you like to generate another password? Enter 'q' to exit. \n")
+        print("Here is your new password: ", done, "\n")
+        q = input("Would you like to generate another password? Enter 'q' to exit. Press any other key to continue. \n")
       
-        if(q == "q"):
+        if(q.lower() == 'q'):
                 QUIT = True
 
         # else:
-        string = ""
+        string = []
         
 print("bye!!!  :)")
